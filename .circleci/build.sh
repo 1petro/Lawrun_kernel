f5ea7fc887749bc739e300afc20920314d0ef99b
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2020-04-13 17:48:27 +0530

    ci: update

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -10,6 +10,9 @@ TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
 export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
 PATH="${PWD}/clang/bin:$PATH"
+CLANG_BIN="${CLANG_DIR}/bin"
+expot CLANG_DIR="clang"
+export LD="${CLANG_BIN}/ld.lld"
 export ARCH=arm64
 export KBUILD_BUILD_HOST=NotKernel
 export KBUILD_BUILD_USER="root"
@@ -51,6 +54,7 @@ function compile() {
    make O=out ARCH=arm64 lavender-perf_defconfig
        make -j$(nproc --all) O=out \
                              ARCH=arm64 \
+                             LD="${LD}" \
 			     CC=clang \
 			     CROSS_COMPILE=aarch64-linux-gnu- \
 			     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
