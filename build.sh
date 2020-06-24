57fd643240463c6d98146ddc8769628d8526fc15
1petro <petronazeh@gmail.com>
2020-06-24 00:04:16 +0000

    Update build script for termux add camera blobs auto-replace and more features

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 rm .version
-
 clear
-
+sed '20,29s/^#//g' -i build.sh; 
 # Resources
 THREAD="-j8"
 KERNEL="Image"
 DTBIMAGE="dtb"
-KERNEL_NAME=sporus-kernel
+KERNEL_NAME=sporus_kernel
 Device_Name=Lavender
-KER_DIR=/root/sporus-kernel
+KER_DIR=/root/sporus_kernel
 out=out-clang
 Kernel_Work=/sdcard/kernel_work/$Device_Name
 ver=10
@@ -17,7 +16,7 @@ ver=10
 echo -e " \e[36m"
 echo "Welcome To Build Script"
 echo -e "\nMade By Petro"
-echo "Do u WanT Build With Gcc Or Clang [1,2]"
+#echo "Do u WanT Build With Gcc Or Clang [1,2]"
 echo -e "[1] Gcc"
 echo -e "[2] Clang"
 echo -ne "\n(i) Make your choice[1-2]: "
@@ -28,7 +27,7 @@ fi
 if [ "$CC_TOOL" == "2" ]; then
 Cc=clang-$ver
 fi
-
+###
 #export CROSS_COMPILE_ARM32=${HOME}/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 chmod 777 AnyKernel3.zip
 mkdir -p /sdcard/kernel_work/$Device_Name
@@ -62,6 +61,7 @@ echo -e "\n[1] Build Kernel"
 	echo -e "[8] Modify Paths/Vars"
 	echo -e "[9] Edit Changelog"
 	echo -e "[10] Quit"
+	echo -e "[11] More..."
 	echo -ne "\n(i) Please enter a choice[1-10]: "
 
 	read choice
@@ -140,16 +140,16 @@ echo "Do u WanT To Check Kernel Log [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]];then
 nano kernel.log
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 else
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 fi
 fi
 if [ "$choice" == "2" ]; then
 echo -e " \e[34m"
 make CC="ccache $Cc" O=$out $DEFCONFIG $THREAD
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 if [ "$choice" == "3" ]; then
 echo -e " \e[31m"
@@ -159,17 +159,17 @@ echo -ne "\n(i) Make your choice[1-2]: "
 read chos
 if [ "$chos" == "1" ]; then
 make CC="ccache $Cc" O=$out menuconfig $THREAD
-./build.sh 
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 if [ "$chos" == "2" ]; then
 nano arch/$ARCH/configs/$DEFCONFIG
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 fi
 if [ "$choice" == "4" ]; then
 echo "Cleaning Source In Progress..."
 rm -r $out
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 if [ "$choice" == "6" ]; then
 echo -e "[1] make DTS only"
@@ -178,14 +178,14 @@ echo -ne "\n(i) Make your choice[1-2]: "
 read Pl
 if [ "$Pl" == "1" ]; then
 make CC="ccache $Cc" O=$out dtbs $THREAD
-./build.sh 
+sed '20,30s/^/#/g' -i build.sh; ./build.sh 
 fi
 if [ "$Pl" == "2" ]; then
 echo "Making DTBO"
 cd /root/libufdt/utils/src 
 python mkdtboimg.py create $Kernel_Work/dtbo.img $KER_DIR/$out/arch/arm64/boot/dts/qcom/*.dtbo
 cd $KER_DIR
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 fi
 if [ "$choice" == "5" ]; then
@@ -207,7 +207,7 @@ git status
 echo "Back To Menu? [Y,n]"
 read S
 if [[ $S == "Y" || $S == "y" ]];then
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 fi
 if [ "$choice" == "8" ]; then
@@ -218,15 +218,52 @@ echo "\nDo U Want To Edit Vars [Y,n]"
 read pp
 if [[ $pp == "Y" || $pp == "y" ]];then
 nano +10,10 build.sh
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 else
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 fi
 if [ "$choice" == "9" ]; then
 nano Changelog_v-2
-./build.sh
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
 fi
 if [ "$choice" == "10" ]; then
 exit
+fi
+if [ "$choice" == "11" ]; then
+echo "Special Add-ons"
+echo -e " \e[31m"
+echo -e "[1] Camera blobs"
+read Pull
+if [ "$Pull" == "1" ]; then
+echo -e "[1] Support For New Blobs Miui11"
+echo -e "[2] Support For Old Blobs Most Roms"
+echo -ne "\n(i) Make your choice[1-2]: "
+read Klk
+if [ "$Klk" == "1" ]; then
+cd 'camera blobs'
+cp msm_isp47.c -r $KER_DIR/drivers/media/platform/msm/camera_v2/isp
+cp msm_isp48.c -r $KER_DIR/drivers/media/platform/msm/camera_v2/isp
+cp msm_sensor.c -r $KER_DIR/drivers/media/platform/msm/camera_v2/sensor
+cp msm_sensor_driver.c -r $KER_DIR/drivers/media/platform/msm/camera_v2/sensor
+cp msm_cam_sensor.h -r $KER_DIR/include/media
+cp camera2.h -r $KER_DIR/include/soc/qcom
+cp msm_camera.h -r $KER_DIR/include/uapi/media
+cp msm_camsensor_sdk.h -r $KER_DIR/include/uapi/media
+cp msmb_camera.h -r $KER_DIR/include/uapi/media
+cd $KER_DIR
+fi
+if [ "$Klk" == "2" ]; then
+git restore drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
+git restore drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
+git restore drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+git restore drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+git restore include/media/msm_cam_sensor.h
+git restore include/soc/qcom/camera2.h
+git restore include/uapi/media/msm_camera.h
+git restore include/uapi/media/msm_camsensor_sdk.h
+git restore include/uapi/media/msmb_camera.h
+sed '20,30s/^/#/g' -i build.sh; ./build.sh
+fi
+fi
 fi
